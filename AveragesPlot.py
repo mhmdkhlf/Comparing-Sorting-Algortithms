a68bc69b0b9e40d392a07e96ad73256b32013022
mhmdkhlf <mohammadkhalifeh1010@gmail.com>
2021-09-19 23:35:05 +0300

    Update AveragesPlot.py

--- AveragesPlot.py
+++ AveragesPlot.py
@@ -1,14 +1,14 @@
-#this is the class that generated the average plot 
+#this is the class that generated the average plot
 
 import matplotlib.pyplot as plt
 
 # the averages in these lists were calculated in the DataVisulazier class
-array_sizes = ['30', '100', '1000', '10000', '100000', '1000000']
-merge_insertion = [None, 140288, 256175, 2599687, 14540166, 1474830932]
-merge = [20833, 119512, 232650, 2535012, 19867166, 1875832015]
-selection = [15462, 147440, 591554, 36227004, 3213359366, 2*10**10]
-insertion = [15716, 201332,  827479, 54606150, 5297283933, 2.5*10**10]
-bubble = [22704, 191992, 1538958, 162662541, 20567778100, None]
+array_sizes = ['100','1000','10000','100000']
+merge_insertion = [58986, 129876, 1642954, 15599237]
+merge = [69831, 227195, 2655326, 18529550]
+selection = [103290, 647814, 35171845, 3216372900]
+insertion = [61650,  400266, 10046600, 882877737]
+bubble = [195231, 1408871, 160709362, 20754095272]
 sorting_algorithms = [merge_insertion, merge, selection, insertion, bubble]
 
 colors = ['black', 'red', 'green', 'blue', 'yellow']
@@ -16,11 +16,12 @@ colors = ['black', 'red', 'green', 'blue', 'yellow']
 def main():
     for i in range(5):
         plt.plot(array_sizes, sorting_algorithms[i], color=colors[i])
-    plt.ylim(0, 8*10**9)
+    plt.ylim(0, 7*10**8)
     plt.xlabel('size of array')
     plt.ylabel('time in nanoseconds')
     plt.title('Averages of sorting algorithms')
-    plt.legend(['merge_insertion',\
+    plt.legend([
+        'merge_insertion',\
                 'merge ',\
                 'selection',\
                 'insertion',\
